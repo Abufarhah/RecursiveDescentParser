a0a9a6f85a7df84bbc319a076394ac08dda8203d
Abufarhah <laythabufarhah@gmail.com>
2021-01-04 01:13:45 +0200

    project skeleton

--- src/edu/birzeit/compiler/Main.java
+++ src/edu/birzeit/compiler/Main.java
@@ -1,4 +1,57 @@
 package edu.birzeit.compiler;
 
+import edu.birzeit.compiler.util.ParserUtil;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner;
+    private static int index=0;
+    private static String token;
+    private static String line;
+    public static void main(String[] args){
+        initFile();
+        readLine();
+        program();
+    }
+
+    private static void initFile(){
+        try {
+            File file=new File("SampleProgram");
+            scanner=new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void readLine(){
+        if(scanner.hasNext()){
+            line=scanner.nextLine();
+        }
+    }
+
+    private static String getToken(){
+//        String temp="";
+//        while (!ParserUtil.isReserved(temp))
+            return null;
+//        TODO: Complete this method
+    }
+
+    private static void program(){
+        getToken();
+        body();
+        if(token.equals("$")){
+            System.out.println("Parsed Successfully!");
+        }else{
+            System.out.println("Error: Missing $");
+        }
+    }
+
+    private static void body(){
+//        libDecl(); TODO: implement this method
+
+    }
+
 }
---
+++ src/edu/birzeit/compiler/util/ParserUtil.java
@@ -0,0 +1,18 @@
+package edu.birzeit.compiler.util;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ParserUtil {
+    private static Set<String> reservedKeyWords = new HashSet(Arrays.asList("$",
+            "main()", "include", "const",
+            "var", "int", "float",
+            "user-defined-name", "float-number", "int-number",
+            "input", "output", "if",
+            "endif", "else", "while"));
+
+    public static boolean isReserved(String s) {
+        return reservedKeyWords.contains(s);
+    }
+}
